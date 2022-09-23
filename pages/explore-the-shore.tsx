f16a7572ee270560a9c21979d3f2a1e8db92bf07
Rob Erskine <rob@roberskine.com>
2022-09-22 22:47:18 -0400

    updating the explore the shore page

--- pages/explore-the-shore.tsx
+++ pages/explore-the-shore.tsx
@@ -22,7 +22,7 @@ const explores = {
         },
         {
           name: 'Manasquan',
-          description: 'Daily badge $10',
+          description: 'Daily badge $10. Oh, this is also where Rob proposed to Molly.',
           link: '',
           image: ''
         },
@@ -42,7 +42,7 @@ const explores = {
       activities: [
         {
           name: 'Silverball Pinball Museum',
-          description: '',
+          description: 'This is the first spot Molly took Rob when he came to visit her hometown. She may have regreted it as they stayed until he played every single Baseball game in the museum.',
           link: 'https://www.silverballmuseum.com/asbury-park/',
           image: ''
         },
@@ -54,7 +54,7 @@ const explores = {
         },
         {
           name: 'Shipwreck Island Mini Golf',
-          description: '',
+          description: 'Rob has yet to beat molly in any mini golf game.',
           link: 'https://bradleybeachminigolf.com/',
           image: ''
         },
@@ -84,7 +84,7 @@ const explores = {
         },
         {
           name: 'The Stone Pony',
-          description: 'Rob and Molly have seen a few shows here, most recently Modest Mouse, one of Rob\'s childhood favorites. Ask Molly\'s brother how the show was!',
+          description: 'Rob and Molly have seen a few shows here, most recently Modest Mouse, one of Rob\'s childhood favorites. Ask Molly\'s brother, Matt, how the show was!',
           link: 'https://www.stoneponyonline.com/',
           image: ''
         },
@@ -178,30 +178,225 @@ const ExploreTheShore: NextPage = () => {
           </p>
         </div>
         
-        <dl className="mt-6 space-y-6 divide-y divide-gray-200">
-          {explores.explore.beaches.map((beach) => (
-            <Disclosure as="div" key={faq.question} className="pt-6">
-              {({ open }) => (
-                <>
-                  <dt className="text-lg">
-                    <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
-                      <span className="font-medium text-gray-900">{faq.question}</span>
-                      <span className="ml-6 flex h-7 items-center">
-                        <ChevronDownIcon
-                          className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </Disclosure.Button>
-                  </dt>
-                  <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                    <p className="text-base text-gray-500">{faq.answer}</p>
-                  </Disclosure.Panel>
-                </>
-              )}
-            </Disclosure>
-          ))}
+        <h2 className="text-3xl font-bold mt-8 max-w-4xl block mx-auto">Explore</h2>
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Beaches</span>
+                      <p className="block w-full text-xl">Here are some of our favorite beaches on the Jersey Shore</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.beaches.map((beach) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{beach?.name}</span>
+                      <span className="text-xl text-gray-400">{beach?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
         </dl>
+
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Activites</span>
+                      <p className="block w-full text-xl">Here are some of our favorite spots near the Wedding Venue</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
+        <h2 className="text-3xl font-bold mt-8 max-w-4xl block mx-auto">EAT</h2>
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Breakfast</span>
+                      <p className="block w-full text-xl">Here&apos;s a short list of our favorite breakfast and brunch spots</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Lunch</span>
+                      <p className="block w-full text-xl">Sammies and other easy bites can be found in here</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Dinner</span>
+                      <p className="block w-full text-xl">Whether you are looking for a dive or a 5-star meal, we got ya covered.</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">Dessert</span>
+                      <p className="block w-full text-xl">Mmm... chocolate.</p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
+        <dl className="mt-4 space-y-6 divide-y divide-gray-200 border-2 border-gray-200 p-4 border-lg rounded-lg max-w-4xl mx-auto">
+          <Disclosure as="div" className="" key="explore">
+            {({ open }) => (
+              <>
+                <dt className="text-lg">
+                  <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
+                    <div>
+                      <span className="block w-full font-medium text-gray-900 text-2xl font-bold">DRANKS</span>
+                      <p className="block w-full text-xl">Molly wouldn&apos;t let me put this one first :( </p>
+                    </div>
+                    <span className="ml-6 flex h-7 items-center mt-4">
+                      <ChevronDownIcon
+                        className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel as="dd" className="mt-2 pr-12 border-t-2 pt-4">
+                  {explores.explore.activities.map((activity) => (
+                    <div className="flex flex-col  pb-4">
+                      <span className="text-2xl">{activity?.name}</span>
+                      <span className="text-xl text-gray-400">{activity?.description}</span>
+                    </div>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        </dl>
+
       </div>
     </div>
   )
